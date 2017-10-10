a9a68856f6a34de4c5fd9d345a9d00f979af7f14
jingjingluyu <1550816456@qq.com>
2017-10-10 09:02:48 +0800

    Linear regression with one variable

--- Machine_Learning/week2/编程作业/gradientDescent.m
+++ Machine_Learning/week2/编程作业/gradientDescent.m
@@ -16,8 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-S= X' *(X*theta-y)
-theta=theta-alpha*S/m
+S= X' *(X*theta-y);
+theta=theta-alpha*S/m;
 
 
 
